233d182a752fd8059c78a75e4e540a916692a551
ihorlihus <ligusigor@ukr.net>
2022-06-07 18:39:16 +0300

    task 2 point 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
 
+
+    const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+
+player.on('currentTime', function (data) {
+        
+    // data is an object containing properties specific to that event
+});
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
