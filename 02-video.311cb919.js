b8acc5867cb5c9c65911a1cf772df260d416dbc5
ihorlihus <93285058+ihorlihus@users.noreply.github.com>
2022-06-07 15:39:54 +0000

    Deploying to gh-pages from @ ihorlihus/goit-js-hw-08@233d182a752fd8059c78a75e4e540a916692a551 🚀

---
+++ 02-video.311cb919.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("iframe"),n=new Vimeo.Player(e);n.on("currentTime",(function(e){})),n.getVideoTitle().then((function(e){console.log("title:",e)}))}();
+//# sourceMappingURL=02-video.311cb919.js.map
---
+++ 02-video.ba21795b.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("iframe"),n=new Vimeo.Player(e);n.on("currentTime",(function(e){})),n.getVideoTitle().then((function(e){console.log("title:",e)}));
+//# sourceMappingURL=02-video.ba21795b.js.map
