2be46dad36f6fbc0c863da1bb8258fadb5958f96
ihorlihus <ligusigor@ukr.net>
2022-06-07 15:28:40 +0300

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallaryInEl = document.querySelector('.gallery');
+const imageLib = { ...galleryItems };
+
+addImages(imageLib)
+function addImages() {
+    const items = [];
+    for (let i = 0; i < galleryItems.length; i += 1) {
+        const item = document.createElement("a");
+        item.classList.add('.gallery__item');
+        item.href = imageLib[i].original;
+        const img = document.createElement("img");
+        img.classList.add('gallery__image');
+        img.src = imageLib[i].preview;
+        img.alt = imageLib[i].description;
+        item.append(img);
+        items.push(item);
+    };
+    gallaryInEl.append(...items);
+};
+const lightbox = new SimpleLightbox('.gallery a',
+    { captionsData: 'alt', captionDelay: 250 });
 // Change code below this line
 
 console.log(galleryItems);
