4a443d98653e457f5025eb2b06032c9fbc9092a7
ihorlihus <ligusigor@ukr.net>
2022-06-10 19:47:14 +0300

    player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,31 @@
 
-
+    import throttle from 'lodash.throttle';
     const iframe = document.querySelector('iframe');
     const player = new Vimeo.Player(iframe);
 
-player.on('currentTime', function (data) {
-        
-    // data is an object containing properties specific to that event
-});
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
+
+player.on('timeupdate', throttle(function (data) {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(data))
+}, 1000)
+);
+
+player.setCurrentTime((JSON.parse(localStorage.getItem("videoplayer-current-time")))
+    .seconds).then(function (seconds) {
+        // seconds = the actual time that the player seeked to
+    }).catch(function (error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
     });
+
+player.ready().then(function() {
+    console.log('the player is ready')
+    player.play()
+});
