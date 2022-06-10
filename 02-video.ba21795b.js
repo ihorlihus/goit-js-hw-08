92775d2b0f6b307b76e02a4b7225a96ceeca7969
ihorlihus <93285058+ihorlihus@users.noreply.github.com>
2022-06-10 16:47:51 +0000

    Deploying to gh-pages from @ ihorlihus/goit-js-hw-08@4a443d98653e457f5025eb2b06032c9fbc9092a7 🚀

--- 02-video.ba21795b.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("iframe"),n=new Vimeo.Player(e);n.on("currentTime",(function(e){})),n.getVideoTitle().then((function(e){console.log("title:",e)}));
-//# sourceMappingURL=02-video.ba21795b.js.map
