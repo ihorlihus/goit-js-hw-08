f2e08d1bfda5aea02b9abe303d7d58908ec8f667
ihorlihus <ligusigor@ukr.net>
2022-06-11 21:33:29 +0300

    begin

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,2 +1,18 @@
-    import throttle from 'lodash.throttle';
+import throttle from 'lodash.throttle';
+    
+const inputEmail = document.querySelector('input');
+const inputText = document.querySelector('.feedback-form textarea')
 
+// inputEmail.addEventListener('input', listenValue());
+inputText.addEventListener('input', listenValue)
+
+console.log(inputText)
+
+function listenValue(event) {
+    const message = event.currentTarget.value;
+    localStorage.setItem("feedback-form-state", message)
+}
+
+function formSubmit(event) {
+    event.preventDefault()
+}
